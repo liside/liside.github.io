b4c06b70e9c6994e579b79f8a9da3cb060ce07e0
Side Li <lisided@gmail.com>
2020-12-28 11:44:43 -0800

    update

--- random/hdfs-3.2.1-setup.sh
+++ random/hdfs-3.2.1-setup.sh
@@ -4,8 +4,8 @@ sudo apt install openjdk-8-jdk -y
 wget https://downloads.apache.org/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz
 tar xvf hadoop-3.2.1.tar.gz
 sudo mv hadoop-3.2.1 /usr/local/hadoop
-sudo adduser --disabled-password -q --gecos "" hdoop
-sudo chown -R hdoop /usr/local/hadoop
+# sudo adduser --disabled-password -q --gecos "" hdoop
+# sudo chown -R hdoop /usr/local/hadoop
 #sudo su - hdoop
 #ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 #cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
@@ -16,23 +16,23 @@ echo "export HADOOP_USER_NAME=hdfs" >> .bashrc
 source .bashrc
 
 if test -b /dev/sdb && ! grep -q /dev/sdb /etc/fstab; then
-    mke2fs -F -j /dev/sdb
-    mount /dev/sdb /mnt
-    chmod 755 /mnt
+    sudo mke2fs -F -j /dev/sdb
+    sudo mount /dev/sdb /mnt
+    sudo chmod 777 -R /mnt
     echo "/dev/sdb	/mnt	ext3	defaults	0	0" >> /etc/fstab
 fi
 
-sudo apt-get install mhddfs
+# sudo apt-get install mhddfs
 sudo mkdir /mnt2
-sudo chmod 777 /mnt2
+sudo chmod 777 -R /mnt2
 sudo mke2fs -F -j /dev/sda4
 sudo mount /dev/sda4 /mnt2
-sudo mkdir /tmpfs
-sudo chmod 777 /tmpfs
-mhddfs /mnt,/mnt2 /tmpfs
+# sudo mkdir /tmpfs
+# sudo chmod 777 /tmpfs
+# mhddfs /mnt,/mnt2 /tmpfs
 
 mkdir /mnt/hadoop
-chmod 1777 /mnt/hadoop
+chmod 777 -R /mnt/hadoop
 
 if ! grep -q fs.defaultFS /usr/local/hadoop/etc/hadoop/core-site.xml; then
 cat > /usr/local/hadoop/etc/hadoop/core-site.xml <<EOF
@@ -91,6 +91,18 @@ cat > /usr/local/hadoop/etc/hadoop/mapred-site.xml <<EOF
   <property>
     <name>mapreduce.jobhistory.webapp.address</name>
     <value>0.0.0.0:19888</value>
+  </property>
+  <property>
+    <name>yarn.app.mapreduce.am.env</name>
+    <value>HADOOP_MAPRED_HOME=/usr/local/hadoop</value>
+  </property>
+  <property>
+    <name>mapreduce.map.env</name>
+    <value>HADOOP_MAPRED_HOME=/usr/local/hadoop</value>
+  </property>
+  <property>
+    <name>mapreduce.reduce.env</name>
+    <value>HADOOP_MAPRED_HOME=/usr/local/hadoop</value>
   </property>
 </configuration>
 EOF
